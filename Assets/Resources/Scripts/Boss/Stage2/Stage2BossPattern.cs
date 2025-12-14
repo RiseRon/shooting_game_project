7dc950dc3c177dd80e98b68784dfeca28d5a0525
KGW0426 <rhdqndyd1324@gmail.com>
2025-12-15 01:55:00 +0900

    추가

--- Assets/Resources/Scripts/Boss/Stage2/Stage2BossPattern.cs
+++ Assets/Resources/Scripts/Boss/Stage2/Stage2BossPattern.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class Stage2BossPattern : MonoBehaviour
 {
     private Stage2BossShoot bossShoot;
+
     private SoundPlayer soundPlayer;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
